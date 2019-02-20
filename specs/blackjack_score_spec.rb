bfccbdcb7606db0c31d00f042da624294b47bf53
Stephanie Marchante <smarcha04@gmail.com>
2019-02-20 13:55:11 -0800

    Added Blackjack score tests

--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -8,7 +8,7 @@ require_relative '../lib/blackjack_score'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe 'Blackjac Score' do
+describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
 
     # Arrange
@@ -18,26 +18,48 @@ describe 'Blackjac Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
 
   it 'facecards have values calculated correctly' do
+    
+    # Arrange
+    facecards = ["King", "Queen", "Jack"]
 
+    facecards.each do |facecard|
+      score = blackjack_score([facecard, 3])
+      expect(score).must_equal 13
+    end
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+    (2..10).each do |card|
+      hand = [card, 1]
 
+      expect(blackjack_score(hand)).must_equal (card + 11)
+    end
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+    hand = ["King", "King", 1]
+    expect(blackjack_score(hand)).must_equal 21
 
+    hand = [10, 4, 1]
+    expect(blackjack_score(hand)).must_equal 15
   end
 
-  it 'raises an ArgumentError for invalid cards' do
 
+  it 'raises an ArgumentError for invalid cards' do
+    hand = ["King", "King", "Princess", 1]
+    expect{
+      (blackjack_score(hand))
+    }.must_equal ArgumentError
   end
 
   it 'raises an ArgumentError for more than 5 cards in the hand' do
-
+    hand = [3, 4, 4, 5, 1, 1]
+    expect{
+      (blackjack_score(hand))
+    }.must_equal ArgumentError
   end
 end
