150292390d4787d3b1c044495e43a97871f2056a
Stephanie Marchante <smarcha04@gmail.com>
2019-02-20 13:55:42 -0800

    blackjack_score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,12 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
+    score = 0
+    hand = 0
 
+    if hand > 5 || hand  < 2
+        raise ArgumentError, "Hand must be valid"
+    end
 end
